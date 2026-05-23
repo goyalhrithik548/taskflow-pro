33349232469ba49095187bd8315f307173665d20
Hrithik Kumar <158841783+goyalhrithik548@users.noreply.github.com>
2026-05-23 16:42:26 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,4 +1,3 @@
-import React from "react";
 import axios from "axios";
 
 const api = axios.create({
@@ -7,9 +6,11 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
@@ -18,10 +19,12 @@ api.interceptors.response.use(
   (error) => {
     if (error.response?.status === 401) {
       localStorage.removeItem("token");
+
       if (!["/login", "/register"].includes(window.location.pathname)) {
         window.location.assign("/login");
       }
     }
+
     return Promise.reject(error);
   }
 );
